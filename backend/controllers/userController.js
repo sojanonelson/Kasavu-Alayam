c0f110dc96b2b658d94bd29cd26ba9daf1f326c6
sojan soj <sojanonelson>
2025-06-26 15:42:11 +0530

    FIXED

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '15s' }
+    { expiresIn: '59m' }
   );
 };
 
